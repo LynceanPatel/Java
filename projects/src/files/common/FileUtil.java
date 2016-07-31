2c42e6c72eb1ec73c9cf5c6f953731acad48b6af
LynceanPatel <LynceanPatel@gmail.com>
2016-07-31 21:22:15 +0530

    Deleted File project, useless

--- projects/src/files/common/FileUtil.java
+++
@@ -1,8 +0,0 @@
-package files.common;
-
-interface IFileUtil {
-	public void createFile(String fileName, String filePath);
-	public void deleteFile(String fileName, String filePath);
-	public void writeFile(String fileName, String filePath);
-	public void readFile(String fileName, String filePath);
-}
